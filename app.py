d2f7444c514ca752c2143312e6541b9a4ea1711a
CorryJ <47356447+CorryJ@users.noreply.github.com>
2025-02-06 11:56:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,37 @@ import pdfplumber
 import openai
 import pandas as pd
 import json
+# Setting page title and header
+st.set_page_config(page_title="The SEO Works AI Bot",  
+                   page_icon="https://www.seoworks.co.uk/wp-content/themes/seoworks/assets/images/fav.png", 
+                   layout="wide",initial_sidebar_state="collapsed")
+
+# Remove whitespace from the top of the page and sidebar
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 0rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("")
+   
+with col2:
+    st.header("")
+    st.image("resources/SeoWorksLogo-Dark.png")
+
+with col3:
+    st.header("")
+
+
+st.markdown('<div style="text-align: center; font-size:24px;"><strong>The SEO Works Ad analyser<strong></div>', unsafe_allow_html=True)
 
 # Initialise OpenAI client
 client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
